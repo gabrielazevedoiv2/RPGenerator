3cf189eae762a57b5a5830f266e448d1c79f5fff
gabrielazevedoiv2 <45005904+gabrielazevedoiv2@users.noreply.github.com>
2019-11-22 11:06:58 -0300

    Update mongoose.ts

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -3,7 +3,7 @@ import * as mongoose from "mongoose";
 export default function start() {
     mongoose.set('useFindAndModify', false);
     mongoose.set('useCreateIndex', true);
-    mongoose.connect("mongodb://155.138.242.116:27017/rpggenerator", { auth:{
+    mongoose.connect("mongodb://ip:port/rpggenerator", { auth:{
         authdb: "admin",
         user: "admin",
         password: "password"
